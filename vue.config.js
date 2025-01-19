5fce0da5bb04b183cd3ebea5aa91d6004f3b9520
alexey2626 <alexej.stadnik@yandex.ru>
2025-01-19 18:29:29 +0400

    dist

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production'
+      ? 'https://github.com/lexaweb/no-code' // Замените на имя вашего репозитория
+      : '/'
 })
